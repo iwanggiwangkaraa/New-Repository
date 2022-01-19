98b90c9ee6cd716640b3093f8626c29b3a2c2cea
iwanggiwangkaraa <91723641+iwanggiwangkaraa@users.noreply.github.com>
2022-01-19 14:30:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//Ganjil & Genap
+function oddEven (nomor){
+    if (nomor % 3 == 0){
+        console.log('Genap')
+    }
+else {
+    console.log('Ganjil')
+    }
+}
+
+oddEven(372738)
+ 
+//Rumus persegi
+function persegi (sisi){
+    console.log(sisi * sisi)
+}
+
+persegi(5, 5)
+
+//Rumus persegi panjang
+function persegipanjang(panjang,lebar){
+    console.log(panjang * lebar)
+}
+
+persegipanjang(25,10)
