06358dd90a862acfb63e643019672b14cf87e752
iwanggiwangkaraa <91723641+iwanggiwangkaraa@users.noreply.github.com>
2022-01-21 13:46:48 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+////perulang for,for in,for off
+const archon = ["Chen","Silencer", "OgreMagi", "Rubick"];
+
+for (let i = 0; i < archon.length; i++) {
+    console.log(archon[i]);
+}
+ 
+const gender = ['Male','Female','Gay'];
+let a = 0;
+for (let a in gender) {
+    console.log(gender[a]);
+}
+
+const nickname = ["iwang","zidan","loy","wikky"];
+for (let x of nickname) {
+    console.log(x);
+}
